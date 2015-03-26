ebd724f263ce604802a8476744694bdb96a2c5a1
simone29111991 <simone.furn@gmail.com>
2015-03-26 15:36:12 +0100

    interfaccia colorata

--- ARDroneTest/Form1.Designer.cs
+++ ARDroneTest/Form1.Designer.cs
@@ -47,10 +47,9 @@
             // 
             // connettiButton
             // 
-            this.connettiButton.Location = new System.Drawing.Point(18, 18);
-            this.connettiButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.connettiButton.Location = new System.Drawing.Point(12, 12);
             this.connettiButton.Name = "connettiButton";
-            this.connettiButton.Size = new System.Drawing.Size(112, 35);
+            this.connettiButton.Size = new System.Drawing.Size(75, 23);
             this.connettiButton.TabIndex = 0;
             this.connettiButton.Text = "CONNETTI";
             this.connettiButton.UseVisualStyleBackColor = true;
@@ -60,146 +59,153 @@
             // 
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.status});
-            this.statusStrip.Location = new System.Drawing.Point(0, 533);
+            this.statusStrip.Location = new System.Drawing.Point(0, 317);
             this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Padding = new System.Windows.Forms.Padding(2, 0, 21, 0);
-            this.statusStrip.Size = new System.Drawing.Size(1066, 30);
+            this.statusStrip.Size = new System.Drawing.Size(668, 22);
             this.statusStrip.TabIndex = 1;
             // 
             // status
             // 
             this.status.Name = "status";
-            this.status.Size = new System.Drawing.Size(181, 25);
+            this.status.Size = new System.Drawing.Size(119, 17);
             this.status.Text = "Drone non connesso.";
             // 
             // takeoffButton
             // 
-            this.takeoffButton.Location = new System.Drawing.Point(18, 102);
-            this.takeoffButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.takeoffButton.BackColor = System.Drawing.Color.LightBlue;
+            this.takeoffButton.Location = new System.Drawing.Point(12, 170);
             this.takeoffButton.Name = "takeoffButton";
-            this.takeoffButton.Size = new System.Drawing.Size(112, 35);
+            this.takeoffButton.Size = new System.Drawing.Size(75, 23);
             this.takeoffButton.TabIndex = 2;
             this.takeoffButton.Text = "Decollo";
-            this.takeoffButton.UseVisualStyleBackColor = true;
+            this.takeoffButton.UseVisualStyleBackColor = false;
             this.takeoffButton.Click += new System.EventHandler(this.takeoffButton_Click);
             // 
             // landButton
             // 
-            this.landButton.Location = new System.Drawing.Point(18, 162);
-            this.landButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.landButton.BackColor = System.Drawing.Color.LightBlue;
+            this.landButton.Location = new System.Drawing.Point(12, 223);
             this.landButton.Name = "landButton";
-            this.landButton.Size = new System.Drawing.Size(112, 35);
+            this.landButton.Size = new System.Drawing.Size(75, 23);
             this.landButton.TabIndex = 3;
             this.landButton.Text = "Atterra";
-            this.landButton.UseVisualStyleBackColor = true;
+            this.landButton.UseVisualStyleBackColor = false;
             this.landButton.Click += new System.EventHandler(this.landButton_Click);
             // 
             // moveUpButton
             // 
+            this.moveUpButton.BackColor = System.Drawing.Color.Silver;
             this.moveUpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.moveUpButton.Location = new System.Drawing.Point(420, 240);
-            this.moveUpButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.moveUpButton.Location = new System.Drawing.Point(339, 77);
             this.moveUpButton.Name = "moveUpButton";
-            this.moveUpButton.Size = new System.Drawing.Size(60, 89);
+            this.moveUpButton.Size = new System.Drawing.Size(40, 58);
             this.moveUpButton.TabIndex = 4;
             this.moveUpButton.Text = "˄";
-            this.moveUpButton.UseVisualStyleBackColor = true;
+            this.moveUpButton.UseVisualStyleBackColor = false;
             this.moveUpButton.Click += new System.EventHandler(this.moveUpButton_Click);
             // 
             // moveDownButton
             // 
+            this.moveDownButton.BackColor = System.Drawing.Color.Silver;
             this.moveDownButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.moveDownButton.Location = new System.Drawing.Point(420, 362);
-            this.moveDownButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.moveDownButton.Location = new System.Drawing.Point(339, 170);
             this.moveDownButton.Name = "moveDownButton";
-            this.moveDownButton.Size = new System.Drawing.Size(60, 89);
+            this.moveDownButton.Size = new System.Drawing.Size(40, 58);
             this.moveDownButton.TabIndex = 5;
             this.moveDownButton.Text = "˅";
-            this.moveDownButton.UseVisualStyleBackColor = true;
+            this.moveDownButton.UseVisualStyleBackColor = false;
             this.moveDownButton.Click += new System.EventHandler(this.moveDownButton_Click);
             // 
             // buttonMoveRight
             // 
+            this.buttonMoveRight.BackColor = System.Drawing.Color.Silver;
             this.buttonMoveRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonMoveRight.Location = new System.Drawing.Point(489, 314);
-            this.buttonMoveRight.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonMoveRight.Location = new System.Drawing.Point(406, 134);
             this.buttonMoveRight.Name = "buttonMoveRight";
-            this.buttonMoveRight.Size = new System.Drawing.Size(87, 62);
+            this.buttonMoveRight.Size = new System.Drawing.Size(58, 40);
             this.buttonMoveRight.TabIndex = 6;
             this.buttonMoveRight.Text = "˃";
-            this.buttonMoveRight.UseVisualStyleBackColor = true;
+            this.buttonMoveRight.UseVisualStyleBackColor = false;
             this.buttonMoveRight.Click += new System.EventHandler(this.buttonMoveRight_Click);
             // 
             // buttonMoveLeft
             // 
+            this.buttonMoveLeft.BackColor = System.Drawing.Color.Silver;
             this.buttonMoveLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonMoveLeft.Location = new System.Drawing.Point(324, 314);
-            this.buttonMoveLeft.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonMoveLeft.Location = new System.Drawing.Point(258, 134);
             this.buttonMoveLeft.Name = "buttonMoveLeft";
-            this.buttonMoveLeft.Size = new System.Drawing.Size(87, 62);
+            this.buttonMoveLeft.Size = new System.Drawing.Size(58, 40);
             this.buttonMoveLeft.TabIndex = 7;
             this.buttonMoveLeft.Text = "˂";
-            this.buttonMoveLeft.UseVisualStyleBackColor = true;
+            this.buttonMoveLeft.UseVisualStyleBackColor = false;
             this.buttonMoveLeft.Click += new System.EventHandler(this.buttonMoveLeft_Click);
             // 
             // buttonGoUp
             // 
-            this.buttonGoUp.Location = new System.Drawing.Point(324, 40);
-            this.buttonGoUp.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonGoUp.BackColor = System.Drawing.Color.RoyalBlue;
+            this.buttonGoUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGoUp.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonGoUp.Location = new System.Drawing.Point(280, 13);
             this.buttonGoUp.Name = "buttonGoUp";
-            this.buttonGoUp.Size = new System.Drawing.Size(252, 52);
+            this.buttonGoUp.Size = new System.Drawing.Size(168, 34);
             this.buttonGoUp.TabIndex = 8;
             this.buttonGoUp.Text = "UP";
-            this.buttonGoUp.UseVisualStyleBackColor = true;
+            this.buttonGoUp.UseVisualStyleBackColor = false;
             this.buttonGoUp.Click += new System.EventHandler(this.buttonGoUp_Click);
             // 
             // buttonGoDown
             // 
-            this.buttonGoDown.Location = new System.Drawing.Point(324, 118);
-            this.buttonGoDown.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonGoDown.BackColor = System.Drawing.Color.RoyalBlue;
+            this.buttonGoDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGoDown.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonGoDown.Location = new System.Drawing.Point(280, 258);
             this.buttonGoDown.Name = "buttonGoDown";
-            this.buttonGoDown.Size = new System.Drawing.Size(252, 52);
+            this.buttonGoDown.Size = new System.Drawing.Size(168, 34);
             this.buttonGoDown.TabIndex = 9;
             this.buttonGoDown.Text = "DOWN";
-            this.buttonGoDown.UseVisualStyleBackColor = true;
+            this.buttonGoDown.UseVisualStyleBackColor = false;
             this.buttonGoDown.Click += new System.EventHandler(this.buttonGoDown_Click);
             // 
             // buttonCalibra
             // 
-            this.buttonCalibra.Location = new System.Drawing.Point(18, 277);
-            this.buttonCalibra.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonCalibra.BackColor = System.Drawing.Color.LightBlue;
+            this.buttonCalibra.Location = new System.Drawing.Point(12, 281);
             this.buttonCalibra.Name = "buttonCalibra";
-            this.buttonCalibra.Size = new System.Drawing.Size(112, 35);
+            this.buttonCalibra.Size = new System.Drawing.Size(75, 23);
             this.buttonCalibra.TabIndex = 10;
             this.buttonCalibra.Text = "Calibra";
-            this.buttonCalibra.UseVisualStyleBackColor = true;
+            this.buttonCalibra.UseVisualStyleBackColor = false;
             this.buttonCalibra.Click += new System.EventHandler(this.buttonCalibra_Click);
             // 
             // rotateLeft
             // 
-            this.rotateLeft.Location = new System.Drawing.Point(713, 144);
+            this.rotateLeft.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.rotateLeft.Location = new System.Drawing.Point(119, 73);
+            this.rotateLeft.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rotateLeft.Name = "rotateLeft";
-            this.rotateLeft.Size = new System.Drawing.Size(96, 41);
+            this.rotateLeft.Size = new System.Drawing.Size(64, 27);
             this.rotateLeft.TabIndex = 11;
             this.rotateLeft.Text = "LR";
-            this.rotateLeft.UseVisualStyleBackColor = true;
+            this.rotateLeft.UseVisualStyleBackColor = false;
             this.rotateLeft.Click += new System.EventHandler(this.rotateLeft_Click);
             // 
             // rotateRight
             // 
-            this.rotateRight.Location = new System.Drawing.Point(825, 144);
+            this.rotateRight.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.rotateRight.Location = new System.Drawing.Point(552, 74);
+            this.rotateRight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rotateRight.Name = "rotateRight";
-            this.rotateRight.Size = new System.Drawing.Size(99, 40);
+            this.rotateRight.Size = new System.Drawing.Size(66, 26);
             this.rotateRight.TabIndex = 12;
             this.rotateRight.Text = "RR";
-            this.rotateRight.UseVisualStyleBackColor = true;
+            this.rotateRight.UseVisualStyleBackColor = false;
             this.rotateRight.Click += new System.EventHandler(this.rotateRight_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1066, 563);
+            this.ClientSize = new System.Drawing.Size(668, 339);
             this.Controls.Add(this.rotateRight);
             this.Controls.Add(this.rotateLeft);
             this.Controls.Add(this.buttonCalibra);
@@ -214,7 +220,6 @@
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.connettiButton);
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "mifalusi";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
